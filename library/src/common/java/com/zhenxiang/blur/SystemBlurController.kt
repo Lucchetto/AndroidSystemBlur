813f1c56d434e1cd20a473b93682d74865beadd1
Lucchetto <lucchetto.tie@live.com>
2022-03-28 20:37:39 +0200

    SystemBlurController: redraw drawable on colour changed
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- library/src/common/java/com/zhenxiang/blur/SystemBlurController.kt
+++ library/src/common/java/com/zhenxiang/blur/SystemBlurController.kt
@@ -129,6 +129,7 @@ class SystemBlurController(
             }
             is ShapeDrawable -> bg.paint.color = backgroundColour
         }
+        bg?.invalidateSelf()
     }
 
     @RequiresApi(Build.VERSION_CODES.S)
